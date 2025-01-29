7dab57c5f028592d7906abbf0fc1a1f23fdaa949
Lukas Martínez <lukasmartinezc@gmail.com>
2025-01-28 21:00:55 -0300

    nav finished

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 const menuList = document.getElementById('menu-list')
 const menuBtn = document.getElementById('menu-btn')
 const closeBtn = document.getElementById('close-btn')
+const nav = document.getElementById('nav-line')
 
 menuList.style.maxHeight = '0px'
 menuList.style.padding = '0px'
-
 
 function toggleMenu() {
     if(menuList.style.maxHeight === '0px') {
@@ -12,10 +12,12 @@ function toggleMenu() {
         menuList.style.padding = '32px 16px'
         menuBtn.style.display = 'none'
         closeBtn.style.display = 'block'
+        nav.style.border = '2px solid #d4d4d4'
     }   else {
         menuList.style.maxHeight = '0px'
         menuList.style.padding = '0px'
         menuBtn.style.display = 'block'
         closeBtn.style.display = 'none'
+        nav.style.border = 'none'
     }
 }
